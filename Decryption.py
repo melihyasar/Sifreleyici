e46b43f4f0e4b396b10bd0cc42d88aa754543f1b
Melih Yaşar <mustafa.melih.ysr@outlook.com>
2021-11-16 21:08:24 +0300

    Şifreleme-Şifreyi çözme mekanizması tamamlandı

--- Decryption.py
+++ Decryption.py
@@ -1,159 +1,165 @@
 
 def decrypt(text1, key):
+    keyr = list(key)[::-1]
+    t = int(keyr[0])
+    c = int(keyr[1])
+    a = int(keyr[2])
+    global text2
+    global text3
+    global text4
+    text2 = int(text1) - t
+    text3 = int(text2) // c
+    text4 = str(text3)
     gtext = ""
-    a_string = text1
-
+    a_string = text4
     split_strings = []
     n  = 2
+    
     for index in range(0, len(a_string), n):
         split_strings.append(a_string[index : index + n])
-
-    if key == 2:
+    
+    if a == 2:
         for p in split_strings:
             if p == "11":
                 gtext= gtext + "a"
-            if p == "12":
+            elif p == "12":
                 gtext= gtext + "b"
-            if p == "13":
+            elif p == "13":
                 gtext= gtext + "c"
-            if p == "14":
+            elif p == "14":
                 gtext= gtext + "ç"
-            if p == "15":
+            elif p == "15":
                 gtext= gtext + "d"
-            if p == "16":
+            elif p == "16":
                 gtext= gtext + "e"
             if p == "17":
                 gtext= gtext + "f"
-            if p == "21":
+            elif p == "21":
                 gtext= gtext + "g"
-            if p == "22":
+            elif p == "22":
                 gtext= gtext + "ğ"
-            if p == "23":
+            elif p == "23":
                 gtext= gtext + "h"
-            if p == "24":
+            elif p == "24":
                 gtext= gtext + "ı"
-            if p == "25":
+            elif p == "25":
                 gtext= gtext + "i"
-            if p == "26":
+            elif p == "26":
                 gtext= gtext + "j"
-            if p == "27":
+            elif p == "27":
                 gtext= gtext + "k"
-            if p == "31":
+            elif p == "31":
                 gtext= gtext + "l"
-            if p == "32":
+            elif p == "32":
                 gtext= gtext + "m"
-            if p == "33":
+            elif p == "33":
                 gtext= gtext + "n"
-            if p == "34":
+            elif p == "34":
                 gtext= gtext + "o"
-            if p == "35":
+            elif p == "35":
                 gtext= gtext + "ö"
-            if p == "36":
+            elif p == "36":
                 gtext= gtext + "p"
-            if p == "37":
+            elif p == "37":
                 gtext= gtext + "r"
-            if p == "41":
+            elif p == "41":
                 gtext= gtext + "s"
-            if p == "42":
+            elif p == "42":
                 gtext= gtext + "ş"
-            if p == "43":
+            elif p == "43":
                 gtext= gtext + "t"
-            if p == "28":
+            elif p == "28":
                 gtext= gtext + "u"
-            if p == "54":
+            elif p == "54":
                 gtext= gtext + "ü"
-            if p == "46":
+            elif p == "46":
                 gtext= gtext + "v"
-            if p == "47":
+            elif p == "47":
                 gtext= gtext + "y"
-            if p == "51":
+            elif p == "51":
                 gtext= gtext + "z"
-            if p == "52":
+            elif p == "52":
                 gtext= gtext + " "
-            if p == "45":
+            elif p == "45":
                 gtext= gtext + "x"
-            if p == "44":
+            elif p == "44":
                 gtext= gtext + "w"
-            if p == "69":
+            elif p == "69":
                 gtext= gtext + "q"
-            if p == "39":
+            elif p == "39":
                 gtext= gtext + "."
         return gtext
-    if key == 1:
+    elif a == 1:
         for p in split_strings:
             if p == "17":
                 gtext= gtext + "a"
-            if p == "47":
+            elif p == "47":
                 gtext= gtext + "b"
-            if p == "12":
+            elif p == "12":
                 gtext= gtext + "c"
-            if p == "24":
+            elif p == "24":
                 gtext= gtext + "ç"
-            if p == "36":
+            elif p == "36":
                 gtext= gtext + "d"
-            if p == "34":
+            elif p == "34":
                 gtext= gtext + "e"
-            if p == "44":
+            elif p == "44":
                 gtext= gtext + "f"
-            if p == "52":
+            elif p == "52":
                 gtext= gtext + "g"
-            if p == "33":
+            elif p == "33":
                 gtext= gtext + "ğ"
-            if p == "15":
+            elif p == "15":
                 gtext= gtext + "h"
-            if p == "46":
+            elif p == "46":
                 gtext= gtext + "ı"
-            if p == "22":
+            elif p == "22":
                 gtext= gtext + "i"
-            if p == "35":
+            elif p == "35":
                 gtext= gtext + "j"
-            if p == "11":
+            elif p == "11":
                 gtext= gtext + "k"
-            if p == "27":
+            elif p == "27":
                 gtext= gtext + "l"
-            if p == "51":
+            elif p == "51":
                 gtext= gtext + "m"
-            if p == "16":
+            elif p == "16":
                 gtext= gtext + "n"
-            if p == "43":
+            elif p == "43":
                 gtext= gtext + "o"
-            if p == "26":
+            elif p == "26":
                 gtext= gtext + "ö"
-            if p == "31":
+            elif p == "31":
                 gtext= gtext + "p"
-            if p == "37":
+            elif p == "37":
                 gtext= gtext + "r"
-            if p == "45":
+            elif p == "45":
                 gtext= gtext + "s"
-            if p == "14":
+            elif p == "14":
                 gtext= gtext + "ş"
-            if p == "25":
+            elif p == "25":
                 gtext= gtext + "t"
-            if p == "41":
+            elif p == "41":
                 gtext= gtext + "u"
-            if p == "23":
+            elif p == "23":
                 gtext= gtext + "ü"
-            if p == "13":
+            elif p == "13":
                 gtext= gtext + "v"
-            if p == "42":
+            elif p == "42":
                 gtext= gtext + "y"
-            if p == "21":
+            elif p == "21":
                 gtext= gtext + "z"
-            if p == "32":
+            elif p == "32":
                 gtext= gtext + " "
-            if p == "53":
+            elif p == "53":
                 gtext= gtext + "x"
-            if p == "39":
+            elif p == "39":
                 gtext= gtext + "w"
-            if p == "28":
+            elif p == "28":
                 gtext= gtext + "q"
-            if p == "69":
+            elif p == "69":
                 gtext= gtext + "."
         return gtext
-yazi = decrypt("5134272215", 1)
-print(yazi)
-input()
-
 
     
   
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from Encryption import encrypt
+from Decryption import decrypt
 txt = input("Birşey yaz: ")
 f = open("Deneme.txt", 'w')
-
 x, y, z= encrypt(txt)
 print(x)
 f.write(str(x))
 f.close()
 if z != 1:
      print("Anahtarınız: " + str(y))
+     print()
      input()
+     yazi1 = decrypt(str(x), str(y))
+     print(yazi1)
+     input()
+
+     
+
+
+
+
+
+
     #Yazan: Mustafa Melih Yaşar
     #deneme123
